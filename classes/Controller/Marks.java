f90d8ba7e127a98c7af3d2169208af5dc82a3d50
kirthisahana <89564460+kirthisahana@users.noreply.github.com>
2023-02-05 17:17:53 +0530

    Update Marks.java

--- classes/Controller/Marks.java
+++ classes/Controller/Marks.java
@@ -20,10 +20,8 @@ public class Marks extends HttpServlet //throws IOException,ServletException,Exc
 		
 
 		String x = request.getParameter("reg");
-		System.out.println("Shabbaaa.... in marks function : reg num : "+ x);
 		response.setContentType("text/html");
         PrintWriter out= response.getWriter();
-        //out.println("shabbbaaaa..... IN DETAILS FUNCTION- REGISTER NUMBER IS: "+x);
          try{
         //StudModel sm= new StudModel();
         out.println("<style> ");
@@ -75,16 +73,6 @@ public class Marks extends HttpServlet //throws IOException,ServletException,Exc
            
        // ResultSet res= sm.getmarks(x);
         System.out.println("in the marks function, result set is received");
-        //out.println("shabbbaaaa..... IN MODIFIED  DETAILS FUNCTION- REGISTER NUMBER IS: "+x);
-        //out.println("<table>");
-        //while(res.next())
-        //{
-          
-         
-        
-        //}
-        //out.println("</table>");
-        //out.println(".........................................................");
         }//try-end
 
         catch(Exception E){System.out.println(E);}
